00c4d1018cfc3522193bc17ddb6ea5f7f5c40e2f
bubu1441006 <hoangdung2.bubu@gmail.com>
2023-10-24 18:45:40 +0700

    Update fenwick.cpp

--- code_files/fenwick.cpp
+++ code_files/fenwick.cpp
@@ -1,19 +1,44 @@
-int bit[N];
+#include "general.cpp"
 
-void Update(int u, int v) {
-    for (int i = u; i <= n; i += i & -i) {
-        bit[i] += v;
+struct BIT {
+    int n, logn;
+    vector<int> bit;
+
+    BIT() = default;
+    BIT(int _n) {
+        n = _n;
+        logn = __lg(n);
+        bit.resize(n + 1);
     }
-}
 
-int Get(int u) {
-    int ans = 0;
-    for (int i = u; i >= 1; i -= i & -i) {
-        ans += bit[i];
+    void upd(int u, int v) {
+        for (int i = u; i <= n; i += i & -i) {
+            bit[i] += v;
+        }
     }
-    return ans;
-}
 
-int Get(int l, int r) {
-    return Get(r) - Get(l - 1);
-}
+    int get(int u) {
+        int ans = 0;
+        for (int i = u; i >= 1; i -= i & -i) {
+            ans += bit[i];
+        }
+        return ans;
+    }
+
+    int get(int l, int r) {
+        if (l > r) return 0;
+        return get(r) - get(l - 1);
+    }
+
+    int lower_bound(int u) {
+        int sum = 0, pos = 0;
+        for (int i = logn; i >= 0; i--) {
+            int nxt = pos + (1 << i);
+            if (nxt <= n && sum + bit[nxt] < u) {
+                sum += bit[nxt];
+                pos = nxt;
+            }
+        }
+        return pos + 1;
+    }
+};
