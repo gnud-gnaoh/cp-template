57067ac76f0793456eea599b54d4b17f9faf364e
bubu1441006 <hoangdung2.bubu@gmail.com>
2023-10-24 18:46:58 +0700

    Update rmq.cpp

--- code_files/rmq.cpp
+++ code_files/rmq.cpp
@@ -1,27 +1,33 @@
-// range minimum queries
-int st[LG][N];
-// int mlg[N]; // optional
+#include "general.cpp"
 
-int Get(int l, int r) {
-    int j = __lg(r - l + 1);
-    return min(st[j][l], st[j][r - (1 << j) + 1]);
-}
+const int INF = 0x3f3f3f3f3f3f3f3f;
 
-void Init() {
-    memset(st, 0x3f, sizeof st);
+struct RMQ {
+    int n, logn;
+    vector<vector<int>> st;
 
-    // mlg[1] = 0;
-    // for (int i = 2; i <= n; i++) {
-    //     mlg[i] = mlg[i / 2] + 1;
-    // }
-
-    for (int i = 1; i <= n; i++) {
-        st[0][i] = a[i];    
+    RMQ() = default;
+    RMQ(int _n) {
+        n = _n;
+        logn = __lg(n + 1);
+        st.resize(logn, vector<int>(n + 1));
     }
-
-    for (int j = 1; j < LG; j++) {
+    RMQ(const vector<int>& a) {
+        n = sz(a) - 1; // 1-index
+        logn = __lg(n + 1);
+        st.resize(logn, vector<int>(n + 1, INF));
         for (int i = 1; i <= n; i++) {
-            st[j][i] = min(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
+            st[0][i] = a[i];
+        } 
+        for (int j = 1; j < logn; j++) {
+            for (int i = 1; i + (1 << j) - 1 <= n; i++) {
+                st[j][i] = min(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
+            }
         }
     }
-}
+
+    int get(int l, int r) {
+        int j = __lg(r - l + 1);
+        return min(st[j][l], st[j][r - (1 << j) + 1]);
+    }
+};
