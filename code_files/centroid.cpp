33ee7a1141adf5f16f99215cf4652d220aa72d53
bubu1441006 <hoangdung2.bubu@gmail.com>
2023-10-24 18:46:05 +0700

    Update centroid.cpp

--- code_files/centroid.cpp
+++ code_files/centroid.cpp
@@ -1,28 +1,32 @@
+#include "general.cpp"
+
+const int N = 1e5 + 5;
+
 vector<int> adj[N];
 
 int sz[N], par[N];
 bool vis[N];
 
-void DfsSize(int u, int p) {
+void dfs_size(int u, int p) {
     sz[u] = 1;
     for (int v : adj[u]) {
         if (v != p && !vis[v]) {
-            DfsSize(v, u);
+            dfs_size(v, u);
             sz[u] += sz[v];
         }
     }
 }
 
-int FindCentroid(int u, int p, int s) {
+int find_centroid(int u, int p, int s) {
     for (int v : adj[u]) {
         if (v != p && !vis[v] && sz[v] * 2 > s) {
-            return FindCentroid(v, u, s);
+            return find_centroid(v, u, s);
         }
     }
     return u;
 }
 
-void DfsCalc(int u, int p, bool cal) {
+void dfs_calc(int u, int p, bool cal) {
     if (cal) {
         // calculate answer
     } else {
@@ -30,28 +34,28 @@ void DfsCalc(int u, int p, bool cal) {
     }
     for (int v : adj[u]) {
         if (v != p && !vis[v]) {
-            DfsCalc(v, u, cal);
+            dfs_calc(v, u, cal);
         }
     }
 }
 
-void BuildCentroid(int u, int p) {
-    DfsSize(u, 0);
+void build_centroid(int u, int p) {
+    dfs_size(u, 0);
 
-    int c = FindCentroid(u, p, sz[u]);
+    int c = find_centroid(u, p, sz[u]);
     vis[c] = 1;
     par[c] = p;
 
     for (int v : adj[c]) {
         if (!vis[v]) {
-            DfsCalc(v, c, 1);
-            DfsCalc(v, c, 0);      
+            dfs_calc(v, c, 1);
+            dfs_calc(v, c, 0);      
         }
     }
     
-    for (auto& [v, w] : adj[c]) {
+    for (int v : adj[c]) {
         if (!vis[v]) {
-            BuildCentroid(v, c);
+            build_centroid(v, c);
         }
     }
 }
