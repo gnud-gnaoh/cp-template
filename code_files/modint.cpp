a13813754cdf9eeecf00db264bed82e2c4486cb0
bubu1441006 <hoangdung2.bubu@gmail.com>
2023-10-24 18:46:50 +0700

    Update modint.cpp

---
+++ code_files/modint.cpp
@@ -0,0 +1,35 @@
+#include "general.cpp"
+
+const int MOD = 1e9 + 7;
+
+struct Mint {
+    int v;
+    explicit operator int() { return v; }
+    Mint() { v = 0; }
+    Mint(long long _v) { v = _v % MOD; v += (v < 0) * MOD; }
+};
+Mint& operator += (Mint& a, const Mint& b) {
+    if ((a.v += b.v) >= MOD) a.v -= MOD;
+    return a;
+}
+Mint& operator -= (Mint& a, const Mint& b) {
+    if ((a.v -= b.v) < 0) a.v += MOD;
+    return a;
+}
+Mint operator + (Mint a, Mint b) { return a += b; }
+Mint operator - (Mint a, Mint b) { return a -= b; }
+Mint operator * (const Mint& a, const Mint& b) { return Mint(1LL * a.v * b.v); }
+Mint& operator *= (Mint& a, Mint b) { a = a * b; }
+Mint pow(Mint a, long long p) {
+    Mint res = 1;
+    while (p) {
+        if (p & 1) res *= a;
+        a *= a;
+        p >>= 1;
+    }
+    return res;
+}
+Mint inv(Mint a) {
+    return pow(a, MOD - 2);
+}
+Mint operator / (Mint a, Mint b) { return a * inv(b); }
