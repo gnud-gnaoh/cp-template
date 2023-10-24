6640f5657f9ab06dcfa2909503a9729dba3838a3
bubu1441006 <hoangdung2.bubu@gmail.com>
2023-10-24 18:46:15 +0700

    Update lichao-tree.cpp

--- code_files/lichao-tree.cpp
+++ code_files/lichao-tree.cpp
@@ -1,29 +1,51 @@
-struct Line {
-    int m, b;
-    int operator() (int x) {
-        return m * x + b;
-    }
-} st[N * 4];
+#include "general.cpp"
 
-void Insert(int x, int lx, int rx, Line seg){
-    if (lx + 1 == rx) {
-        if (seg(lx) > st[x](lx)) st[x] = seg;
-        return;
+struct LichaoSegmentTree {
+    struct Line {
+        int m, b;
+        int operator() (int x) {
+            return m * x + b;
+        }
+    };
+
+    int n;
+    vector<Line> st;
+
+    LichaoSegmentTree() = default;
+    LichaoSegmentTree(int _n) {
+        n = _n;
+        st.resize(4 * n + 5);
     }
 
-    int m = (lx + rx) / 2;
-    if (st[x].m > seg.m) swap(seg, st[x]);
-    if (st[x](m) < seg(m)) {
-        swap(st[x] , seg);
-        Insert(2 * x + 1, lx, m, seg);
-    } else {
-        Insert(2 * x + 2, m, rx, seg);
-    }
-}
+    void upd(int x, int lx, int rx, Line seg){
+        if (lx + 1 == rx) {
+            if (seg(lx) > st[x](lx)) st[x] = seg;
+            return;
+        }
 
-int Query(int x, int lx, int rx, int i){
-    if (lx + 1 == rx) return st[x](i);
-    int m = (lx + rx) / 2;
-    if (i < m) return max(st[x](i), Query(2 * x + 1, lx, m, i));
-    else return max(st[x](i), Query(2 * x + 2, m, rx, i));
-}
+        int m = (lx + rx) / 2;
+        if (st[x].m > seg.m) swap(seg, st[x]);
+        if (st[x](m) < seg(m)) {
+            swap(st[x] , seg);
+            upd(2 * x + 1, lx, m, seg);
+        } else {
+            upd(2 * x + 2, m, rx, seg);
+        }
+    }
+
+    int get(int x, int lx, int rx, int i){
+        if (lx + 1 == rx) return st[x](i);
+        int m = (lx + rx) / 2;
+        if (i < m) return max(st[x](i), get(2 * x + 1, lx, m, i));
+        else return max(st[x](i), get(2 * x + 2, m, rx, i));
+    }
+};
+
+/*
+Insert line f(x) = mx + b
+Query for max f(t) for all lines
+
+Negating both m and b for min instead of max
+Be careful: [lx, rx)
+Remember to insert Line(0, 0) or something before doing queries
+*/
