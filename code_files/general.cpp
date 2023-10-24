7658c1d9e584dbd9643175ad276aa8829b8c9089
bubu1441006 <hoangdung2.bubu@gmail.com>
2023-10-24 18:45:25 +0700

    Update general.cpp

--- code_files/general.cpp
+++ code_files/general.cpp
@@ -1,27 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
 
-using ll = long long;
-using ld = long double;
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
 
-#define pii pair<int, int>
-#define x first
-#define y second
-
-const int INF = 0x3f3f3f3f;
-const ll LLINF = 0x3f3f3f3f3f3f3f3f;
-
-void FileIO() {
-    #define TASK ""
-    if (fopen(TASK".inp", "r")) {
-        freopen(TASK".inp", "r", stdin);
-        freopen(TASK".out", "w", stdout);
-    }
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    
 }
