f5b9e011574f3ee67cd8f15239ca88f3652ec530
bubu1441006 <hoangdung2.bubu@gmail.com>
2023-10-24 19:42:13 +0700

    Add string hashing

---
+++ code_files/hash.cpp
@@ -0,0 +1,47 @@
+#include "general.cpp"
+#include "modint.cpp"
+
+const int N = 1e6 + 5;
+const int P = 331; // base
+// big prime mod 1e11 + 7
+
+int op[N];
+Mint mp[N];
+
+void compute_pow() {
+    op[0] = 1;
+    mp[0] = 1;
+    for (int i = 1; i < N; i++) {
+        op[i] = op[i - 1] * P;
+        mp[i] = mp[i - 1] * P;
+    }
+}
+
+struct Hash {
+    int n;
+    vector<int> oh;
+    vector<Mint> mh;
+
+    Hash() = default;
+    Hash(const string& a) {
+        n = sz(a); // a is 0-indexed
+        for (int i = 1; i <= n; i++) {
+            oh[i] = oh[i - 1] * P + a[i - 1];
+            mh[i] = mh[i - 1] * P + a[i - 1];
+        }
+    }
+
+    pair<int, Mint> get(int l, int r) {
+        return {oh[r] - oh[l - 1] * op[r - l + 1], mh[r] - mh[l - 1] * mp[r - l + 1]};
+    }
+};
+
+pair<int, Mint> get_single_hash(const string& a) {
+    int oh;
+    Mint mh;
+    for (int i = 0; i < sz(a); i++) {
+        oh = oh * P + a[i];
+        mh = mh * P + a[i];
+    }
+    return {oh, mh};
+}
